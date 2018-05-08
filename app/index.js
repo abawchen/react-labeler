23fd92cb8a71d1c22e06079d168379dc31befd8b
abawchen <abaw.chen@gmail.com>
2018-05-08 17:51:56 +0800

    First setup

---
+++ app/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+    };
+  }
+  render() {
+    return (
+      <div>
+        <h1>Hello, World!</h1>
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById('app'));
