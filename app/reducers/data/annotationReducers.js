98808c513ae83b422db57153043f92e05990544c
AbawChen <abaw.chen@gmail.com>
2018-05-15 07:26:44 +0800

    try

--- app/reducers/data/annotationReducers.js
+++ app/reducers/data/annotationReducers.js
@@ -5,22 +5,26 @@ import {
   CRAETE
 } from '../../constants/actionTypes';
 
+let coords = {};
+
 const annotatorReducers = handleActions(
   {
     SELECT_POINT: (state, { payload }) => {
       let event = payload.event;
       let aix = event.currentTarget.dataset.aix;
       let pix = event.currentTarget.dataset.pix;
+      coords.x = event.pageX;
+      coords.y = event.pageY;
       return state
         .set('aix', aix)
-        .set('pix', pix)
-        .set('coords', Immutable.fromJS([event.pageX, event.pageY]));
+        .set('pix', pix);
     },
     DESELECT_POINT: (state, { payload }) => {
+      coords = {};
       return state
         .set('aix', -1)
-        .set('pix', -1)
-        .remove('coords');
+        .set('pix', -1);
+
         // .set('coords', Immutable.fromJS([]));
     },
     MOVE_POINT: (state, { payload }) => {
@@ -29,18 +33,18 @@ const annotatorReducers = handleActions(
       let aix = event.currentTarget.dataset.aix;
       let pix = event.currentTarget.dataset.pix;
       // if (aix == state.get('aix') && pix == state.get('pix')) {
-      if (state.has('coords')) {
+      if (!!coords.x) {
         let point = state
           .getIn(['annotations', aix, 'points', pix]);
-        let xDiff = state.getIn(['coords', 0]) - event.pageX;
-        let yDiff = state.getIn(['coords', 1]) - event.pageY;
-        
+        let xDiff = coords.x - event.pageX;
+        let yDiff = coords.y - event.pageY;
+        coords.x = event.pageX;
+        coords.y = event.pageY;
         let points = state
           .getIn(['annotations', aix, 'points'])
           .setIn([pix, 0], point.get(0) - xDiff)
           .setIn([pix, 1], point.get(1) - yDiff);
         state = state
-          .set('coords', Immutable.fromJS([event.pageX, event.pageY]))
           .setIn(['annotations', aix, 'points'], points);
       }
       return state;
