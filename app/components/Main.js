2246c848e6cb9ef3e07680a284b1e0236208b39a
abawchen <abaw.chen@gmail.com>
2018-05-24 15:11:52 +0800

    feat: Introduce react-redux-toastr

--- app/components/Main.js
+++ app/components/Main.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {toastr} from 'react-redux-toastr';
 import ImageContainer from '../containers/ImageContainer';
 
 const Main = () => (
   <div>
     <ImageContainer/>
+   <button
+          onClick={() => toastr.success('The title', 'The message')}
+          type="button">Toastr Success</button>
   </div>
 );
 
--- app/reducers/index.js
+++ app/reducers/index.js
@@ -1,8 +1,10 @@
-import { combineReducers } from 'redux-immutable';
+import {combineReducers} from 'redux-immutable';
+import {reducer as toastrReducer} from 'react-redux-toastr'
 import annotator from './data/annotationReducers';
 
 const rootReducer = combineReducers({
-  annotator
+  annotator,
+  toastr: toastrReducer
 });
 
 export default rootReducer;
