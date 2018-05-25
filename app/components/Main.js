1f63b9fb9f27b2b479dfee18bcc715f3a2d8f727
abawchen <abaw.chen@gmail.com>
2018-05-25 16:09:11 +0800

    feat: Add toast message into PreRectangle

--- app/components/Main.js
+++ app/components/Main.js
@@ -1,14 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {toastr} from 'react-redux-toastr';
 import ImageContainer from '../containers/ImageContainer';
 
 const Main = () => (
   <div>
     <ImageContainer/>
-   <button
-          onClick={() => toastr.success('The title', 'The message')}
-          type="button">Toastr Success</button>
   </div>
 );
 
--- app/components/PreRectangle.js
+++ app/components/PreRectangle.js
@@ -1,53 +1,76 @@
 import React from 'react';
-import { getPathD } from '../utils/annotation';
+import PropTypes from 'prop-types';
+import {toastr} from 'react-redux-toastr';
+import {getPathD} from '../utils/annotation';
 
-const PreRectangle = ({
-  imageWidth,
-  imageHeight,
-  aix,
-  pix,
-  preAnnotation,
-  onPreAnnotationMouseDown,
-  onPreAnnotationMouseUp,
-  onPreAnnotationMouseMove,
-}) => (
-  <g>
-    <path
-      className='ori'
-      fill='gray'
-      fill-rule='evenodd'
-      opacity='0.5'
-      d={getPathD(imageWidth, imageHeight, true, preAnnotation.points)}
-    />
-    <rect
-      opacity={0}
-      width={imageWidth}
-      height={imageHeight}
-      onMouseDown={onPreAnnotationMouseDown}
-      onMouseMove={!!preAnnotation.points.length ? onPreAnnotationMouseMove : null}
-    />
-    <polygon
-      points={preAnnotation.points}
-      // XXXX: Not work in rect element!?
-      onMouseUp={onPreAnnotationMouseUp}
-    />
-    {
-      preAnnotation.points.map((point, index) =>
-        <circle
-          r={pix === index ? 8 : 5}
-          key={index}
-          data-aix={aix}
-          data-pix={index}
-          cx={point[0]}
-          cy={point[1]}
-          style={{
-            fill: pix === index ? 'lime' : 'white',
-            stroke: pix === index ? 'blue' : 'black',
-          }}
-        />
+export default class PreRectangle extends React.Component {
+  static propTypes = {
+    imageWidth: PropTypes.number.isRequired,
+    imageHeight: PropTypes.number.isRequired,
+    aix: PropTypes.number.isRequired,
+    pix: PropTypes.number.isRequired,
+    preAnnotation: PropTypes.object.isRequired,
+    onPreAnnotationMouseDown: PropTypes.func.isRequired,
+    onPreAnnotationMouseUp: PropTypes.func.isRequired,
+    onPreAnnotationMouseMove: PropTypes.func.isRequired,
+  }
+
+  componentDidMount() {
+    const toastrOptions = {
+      component: (
+        <span>
+          1. Drag and drop to annotate.<br/>
+          2. Or press `ESC` to abort.
+        </span>
       )
     }
-  </g>
-)
+    toastr.info('Rectangle Annotation', toastrOptions);
+  }
 
-export default PreRectangle;
+  render() {
+    return (
+      <g>
+        <path
+          className='ori'
+          fill='gray'
+          fill-rule='evenodd'
+          opacity='0.5'
+          d={getPathD(
+              this.props.imageWidth,
+              this.props.imageHeight,
+              true,
+              this.props.preAnnotation.points
+          )}
+        />
+        <rect
+          opacity={0}
+          width={this.props.imageWidth}
+          height={this.props.imageHeight}
+          onMouseDown={this.props.onPreAnnotationMouseDown}
+          onMouseMove={!!this.props.preAnnotation.points.length ? this.props.onPreAnnotationMouseMove : null}
+        />
+        <polygon
+          points={this.props.preAnnotation.points}
+          // XXXX: Not work in rect element!?
+          onMouseUp={this.props.onPreAnnotationMouseUp}
+        />
+        {
+          this.props.preAnnotation.points.map((point, index) =>
+            <circle
+              key={index}
+              r={this.props.pix === index ? 8 : 5}
+              data-aix={this.props.aix}
+              data-pix={index}
+              cx={point[0]}
+              cy={point[1]}
+              style={{
+                fill: this.props.pix === index ? 'lime' : 'white',
+                stroke: this.props.pix === index ? 'blue' : 'black',
+              }}
+            />
+          )
+        }
+      </g>
+    )
+  }
+}
